4141618a728136b56d5b5a0acbab0f2645961caf
Andrew Hyde <andrewryanhyde@hotmail.co.uk>
2018-05-22 11:32:44 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -11,6 +11,25 @@ const codes = [
   "a"
 ];
 
+
+chai.config.includeStack = true;
 function init() {
-  // your code here
-}
+  
+  let index = 0;
+	document.body.addEventListener('keydown', function event(e)
+	{
+	  let key = e.key;
+	  console.log(e.key)
+	  if (key === codes[index]) {
+	    index++;
+	  }
+	  
+	  if (index === codes.length) {
+	    alert("Congratulations, you entered the Konami code!");
+
+	    index = 0;
+	    
+	  } else {
+	    index = 0;
+	  }
+	 })}
