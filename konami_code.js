b48dd415e4cbe937365ee42b6be20ba8e0c53afc
Andrew Hyde <andrewryanhyde@hotmail.co.uk>
2018-05-22 11:39:30 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -15,26 +15,7 @@ const codes = [
 chai.config.includeStack = true;
 
 
-function init() {
-  let index=0;
-function init() {
-  document.body.addEventListener("keydown", function event(e){
-   	  let key = e.key;
-	  console.log(e.key)
-	  if (key === codes[index]) {
-	    index++;
-	  }
-     if (index === codes.length) {
+ if (index === codes.length) {
 	    alert("Congratulations, you entered the Konami code!");
 
 	    index = 0;
-
-      }
-
-    else {
-      index=0;
-    }
-  })
-}
-init();
-}
